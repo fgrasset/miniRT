50069575d8f07b2ac16d291d00ac35a8431ecdd2
Fabien Grasset <fabien.grasset.99@gmail.com>
2023-07-10 14:57:18 +0200

    creating rays might work, have not tested it yet but it should be finished

--- srcs/ray.c
+++ srcs/ray.c
@@ -119,14 +119,28 @@ t_v3d	make_v_dir(t_rt *rt, double x, double y)
 	double	screen_y;
 	double	v_fov;
 	double	h_fov;
+	double	screen_distance;
+	t_v3d	s_center;
 
 	screen_x = (x / (double)WIN_W) * 2 -1;
 	screen_y = (y / (double)WIN_H) * 2 -1;
 	v_fov = rt->sc->cam.fov_h * M_PI / 180.0;
 	h_fov = 2 * atan(tan(v_fov / 2) * (WIN_W / WIN_H));
 
+	//calculate the distance to screen
+	screen_distance = (WIN_H / 2) / tan(v_fov / 2);
+
+	//calculate the screen center
+	s_center.x = rt->sc->cam.coord.x + rt->sc->cam.dir.x * screen_distance;
+	s_center.y = rt->sc->cam.coord.y + rt->sc->cam.dir.y * screen_distance;
+	s_center.z = rt->sc->cam.coord.z + rt->sc->cam.dir.z * screen_distance;
+
 	//scale the pixel to the screen dimensions
-	screen_pos->x =
+	screen_pos->x = s_center.x + x * tan(h_fov / 2);
+	screen_pos->y = s_center.y + y * tan(v_fov / 2) / ((double)WIN_W / (double)WIN_H);
+	screen_pos->z = s_center.z;
+
+	v_dir = sub(screen_pos, rt->sc->cam.coord);
 
 	return(normalize(v_dir));
 }
