bfed785073991138d3470f13c0934ec937dd6df3
TheBrisly <lfabbian@student.42lausanne.ch>
2023-08-16 09:43:44 +0200

    malloc modif

--- srcs/utils/malloc.c
+++ srcs/utils/malloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   malloc.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgrasset <fgrasset@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 09:32:38 by fgrasset          #+#    #+#             */
-/*   Updated: 2023/08/16 09:37:06 by fgrasset         ###   ########.fr       */
+/*   Updated: 2023/08/16 09:43:17 by lfabbian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_inter	*calloc_utils(void)
 {
-	t_inter *new;
+	t_inter	*new;
 
 	new = ft_calloc(sizeof(t_inter), 1);
 	if (!new)
